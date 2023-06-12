87385ee589f4096f66c25cd3905b2e2e0d0454a0
dande100 <vasanthid10@gmail.com>
2023-06-12 15:06:39 -0500

    Random text generate onload

--- src/app.js
+++ src/app.js
@@ -4,8 +4,29 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
-
+let who = ["The dog", "My grandma", "His turtle", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "the keys", "the car"];
+let when = [
+  "before the class",
+  "right on time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let excuseEl = document.getElementById("excuse");
+  const whoRandomText = Math.floor(Math.random() * who.length);
+  const actionRandomText = Math.floor(Math.random() * action.length);
+  const whatRandomText = Math.floor(Math.random() * what.length);
+  const whenRandomText = Math.floor(Math.random() * when.length);
+  excuseEl.innerHTML =
+    who[whoRandomText] +
+    " " +
+    action[actionRandomText] +
+    " " +
+    what[whatRandomText] +
+    " " +
+    when[whenRandomText];
 };
